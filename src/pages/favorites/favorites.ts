1cd914f4fa2401ca5c7cc184ce662fae9315b287
jnavarro <justinn@liveanswer.com>
2017-07-25 17:04:32 -0400

    preparing the favorite quotes page

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {Quote} from '../../data/quote.interface';
+import {QuotesService} from '../../services/quotes.service';
+
 
 @Component({
   selector: 'page-favorites',
   templateUrl: 'favorites.html',
 })
 export class FavoritesPage {
-	
+	quotes: Quote[];
+
+	constructor(private quotesService: QuotesService) {}
+
+	ionViewWillEnter() {
+		this.quotes = this.quotesService.getFavoriteQuotes();
+	}
 }
