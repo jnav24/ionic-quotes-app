caf81235a6c3425b956afc56a04c7e2c313e6ab6
jnavarro <justinn@liveanswer.com>
2017-07-25 08:20:42 -0400

    add alerts functionality

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NavParams} from 'ionic-angular';
+import {NavParams, AlertController} from 'ionic-angular';
 import {Quote} from '../../data/quote.interface';
 
 @Component({
@@ -9,9 +9,34 @@ import {Quote} from '../../data/quote.interface';
 export class QuotesPage {
 	quoteGroup: {category: string, quotes: Quote[], icon: string}[];
 
-	constructor(private navParams: NavParams) {}
+	constructor(private navParams: NavParams, private alertCtrl: AlertController) {}
 
 	ionViewDidLoad() {
 		this.quoteGroup = this.navParams.data;
+	}
+
+	onAddToFavorite(selectedQuote: Quote) {
+		const alert = this.alertCtrl.create({
+			title: 'Add Quote',
+			subTitle: 'Are you sure?',
+			message: 'Are you sure you want to add the quote?',
+			buttons: [
+				{ 
+					text: 'Yes, go ahead', 
+					handler: () => {
+						console.log('ok');
+					}
+				},
+				{ 
+					text: 'No, thanks',
+					role: 'cancel',
+					handler: () => {
+						console.log('canceled');
+					}
+				},
+			]
+		});
+
+		alert.present();
 	}
 }
