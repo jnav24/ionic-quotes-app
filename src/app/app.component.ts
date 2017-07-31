aff9526ba83c8931c10e062ed13c150d82fc0479
jnavarro <justinn@liveanswer.com>
2017-07-31 07:51:55 -0400

    change the root to set up menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { NavController, MenuController } from 'ionic-angular';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { SettingsPage } from '../pages/settings/settings';
@@ -13,8 +14,9 @@ export class MyApp {
   // rootPage:any = TabsPage;
   tabsPage = TabsPage;
   settingsPage = SettingsPage;
+  @ViewChild('nav') nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,  private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -24,7 +26,8 @@ export class MyApp {
   }
 
   onLoad(page: any) {
-
+      this.nav.setRoot(page);
+      this.menuCtrl.close();
   }
 }
 
